10b61349898649a7ffebadd673312f224650c259
VincentGeranium <kwangjun3952@gmail.com>
2021-04-18 16:16:28 +0900

    Setting up the TikTok Clone project for iOS

---
+++ TikTok_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok_Clone
+//
+//  Created by 김광준 on 2021/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+

2226f69f3b0294f22fa51f4f29918193e1b2a10d
VincentGeranium <kwangjun3952@gmail.com>
2021-04-21 16:26:55 +0900

    setup all manager classes

--- TikTok_Clone/Managers/APICaller.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APICaller.swift
-//  TikTok_Clone
-//
-//  Created by 김광준 on 2021/04/18.
-//
-
-import Foundation
--- TikTok_Clone/Managers/DatabaseManager.swift
+++ TikTok_Clone/Managers/DatabaseManager.swift
@@ -4,5 +4,22 @@
 //
 //  Created by 김광준 on 2021/04/18.
 //
+// 데이터베이스로 부터 여러 정보를 받아오기 위한 것.
+
 
 import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    public static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+    private init() {}
+    
+    // Public
+    
+    public func getAllUsers(completion: ([String]) -> Void) {
+        
+    }
+}
